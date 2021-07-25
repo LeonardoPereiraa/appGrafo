e7a112b458eaf1f034b368a1bcbef7090e301810
Leonardo <leonardopereiradossantos@id.uff.br>
2021-07-25 11:40:43 -0300

    de atualizacao

--- estrutura.py
+++ estrutura.py
@@ -5,20 +5,27 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=200, height=200, borderwidth=0, highlightthickness=0, bg="black")
 canvas.grid()
 class No :
-    def __init__(self,nome,peso,canva):
+    def __init__(self,nome,peso,canva, x, y):
         self.canva = canva
         self.nome = nome
         self.peso = peso
         self.arestas = []
+        self.desenharNo(x,y)
     def desenharNo(self, x , y):
         self.x = x
         self.y = y
         self.canvaNoId = canvas.create_oval(self.x, self.y,self.x +20, self.y +20,fill="#BBB",outline="") 
         self._desenharPeso()
-    def _desenharPeso(self):
-         
+    def _desenharPeso(self):         
         self.pesoId = canvas.create_text(self.x + 10,self.y + 10,text=self.peso,fill="yellow")
-       
+
+    def apagarDesenhoNo(self):
+        self.apagarDesenhePeso()
+        self.canva.delete(self.canvaNoId)
+        self.canvaNoId=None
+    def apagarDesenhePeso(self):
+        self.canva.delete(self.pesoId)
+        self.pesoId =None
     def addAresta(self,aresta):
         self.arestas.append(aresta)
 ##  remove apenas uma das aresta do no 
@@ -36,7 +43,16 @@ class No :
         tamanho = len(self.arestas)
         for i in range(tamanho) :
             self.removeAresta(self.arestas[0].no1.nome,self.arestas[0].no2.nome)
+    def apagarNo(self):
+        self.removeAllAresta()
+        self.apagarDesenhoNo()
+        self.canva = None
+        self.nome = None
+        self.peso = None
+        self.arestas = None
 
+
+## classe da aresta
 class Aresta:
     def __init__(self,no1,no2,canva):
         self.canva = canva
@@ -46,6 +62,7 @@ class Aresta:
         self.no1.addAresta(self)
         self.no2.addAresta(self)
         self.NumeroDeNosDeletado=0
+        self.desenharAresta()
 
 ## essa função so deve ser usada pelo No quado for preciso remover uma aresta
     def apagarAresta(self,no):
@@ -53,20 +70,32 @@ class Aresta:
             self.no1 = None
             self.no2 = None
             self.NumeroDeNosDeletado=None
+            self.apagarDesenhoAresta()
             return
         if self.no1 != no :
             self.no1.removeAresta(self.no1.nome,self.no2.nome)
             return
         if self.no2 != no :
             self.no2.removeAresta(self.no1.nome,self.no2.nome)
+    def desenharAresta(self):
+        self.no1.apagarDesenhoNo()
+        self.no2.apagarDesenhoNo()
+        self.idDesenhoAresta = canvas.create_line(self.no1.x+10, self.no1.y+10, self.no2.x+10, self.no2.y+10,fill="#fff",width =3)
+        self.no1.desenharNo(self.no1.x,self.no1.y)
+        self.no2.desenharNo(self.no2.x,self.no2.y)
 
+    def apagarDesenhoAresta(self):
+        self.canva.delete(self.idDesenhoAresta)
+        self.idDesenhoAresta= None
+
+# class espece
 class especeGrafo:
     def __init__(self):
         self.espece = {}
 ## adiciona um nó ao espaço de grafos
-    def addNo(self,no1):
-        if self.espece.get(no1.nome) is None :
-            self.espece[no1.nome]=no1
+    def addNo(self,noId,peso,px,py,canvas):
+        if self.espece.get(noId) is None :
+            self.espece[noId]= No(noId,peso,canvas,px,py)
             return
         return
 ## procura um nó do espaço de grafos
@@ -75,7 +104,7 @@ class especeGrafo:
 ## remove um nó do espaço de grafos
     def removerNo(self, id):
         if self.espece.get(id) is not None :
-            self.espece[id].removeAllAresta()
+            self.espece[id].apagarNo()
             del(self.espece[id])
             return
         return
@@ -84,18 +113,51 @@ class especeGrafo:
         if self.espece.get(id1) is not None :
             self.espece[id1].removeAresta(id1,id2)
 ## adiciona uma areste a um par de nós do espaço de grafo
-    def addAresta(self,id1,id2):
+    def addAresta(self,id1,id2,canvas):
         if self.espece.get(id1) is not None and self.espece.get(id2) is not None :
-            Aresta(self.espece.get(id1),self.espece.get(id2))
+            Aresta(self.espece.get(id1),self.espece.get(id2),canvas)
             return
         return
-c = No("leo",1, canvas)
-c.desenharNo(0,0)
-b= No("leo",2, canvas)
-b.desenharNo(50,0)
-a = No("leo",3, canvas)
-a.desenharNo(100,0)
-e = No("leo",4, canvas)
-e.desenharNo(100,40)
 
-root.mainloop()
+e=especeGrafo()
+#e.addNo("l",2,20,20,canvas)
+#e.addNo("f",2,80,70,canvas)
+#e.addNo("a",2,80,120,canvas)
+#e.addNo("b",2,80,170,canvas)
+#e.addAresta("l","f",canvas)
+#e.addAresta("l","a",canvas)
+#e.addAresta("l","b",canvas)
+#e.removerAresta("l","f")
+#e.removerNo("l")
+sair = False
+while not sair:
+
+    s = input('entrada 0 a 4 > ')
+    if s == "0":
+        sNome = input('nome do nó para remover > ')
+        e.removerNo(sNome)
+        root.update()
+    if s == "1":
+        sNome = input('nome do nó para criar > ')
+        speso = input('peso do nó  > ')
+        sX = int(input('coordenadaX do nó  > '))
+        sY = int(input('coordenadaY do nó  > '))
+        e.addNo(sNome,speso,sX,sY,canvas)
+        root.update()
+    if s == "2":
+        sNome1 = input('nome do primeiro nó da aresta > ')
+    
+        sNome2 = input('nome do segundo nó da aresta > ')
+        e.addAresta(sNome1,sNome2,canvas)
+        root.update()
+    if s == "3":
+
+        sNome1 = input('nome do primeiro nó da aresta para remover > ')
+    
+        sNome2 = input('nome do segundo nó da aresta para remover  > ')
+        e.removerAresta(sNome1,sNome2)
+        root.update()
+    if(s=="4"):
+        sair = True
+#while True :    
+#root.mainloop()
